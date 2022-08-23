5c88a3bed6fc39857683359bc253b29cab51d9c2
AhmedBouafif <ahmedbouafif500@gmail.com>
2022-08-23 10:30:18 +0100

    initial state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App"></div>;
+  return <div className="text-pink-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur animi, totam ipsa corrupti quibusdam delectus temporibus est, cupiditate debitis numquam autem voluptatem facilis nesciunt atque pariatur ea adipisci! Odit, quae!</div>
 }
 
-export default App;
+export default App
