b6bb5eb5d38908a92a038198edb8ba37799edf77
wassim triki <wsmtriki@gmail.com>
2022-08-23 11:23:17 +0100

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { setUser } from './redux/features/authSlice.js';
 import { useEffect } from 'react';
-import Home from '../pages/Home.js';
+import Home from './pages/Home.js';
 import Modal from 'react-modal';
 import Signup from './pages/Signup';
 Modal.setAppElement('#root');
 const App = () => {
-  const dispatch = useDispatch();
-  const user = JSON.parse(localStorage.getItem('userProfile'));
-  useEffect(() => {
-    dispatch(setUser(user));
-  });
   return (
     <BrowserRouter>
       <Signup />
