4edbadaf515817db4615387e87bbad28366f60f1
wassim triki <wsmtriki@gmail.com>
2022-08-23 10:40:45 +0100

    begin signup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form className="bg-red-500 flex flex-col items-center">
+        <input type="email" placeholder="Your email" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
