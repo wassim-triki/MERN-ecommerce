cb779cb0e7830074bc91e85f1c71e721b9f8648d
wassim triki <wsmtriki@gmail.com>
2022-08-23 12:25:21 +0100

    add helpers customestyles modal

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,22 +3,8 @@ import AuthInput from '../components/AuthInput';
 import Modal from 'react-modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { signup } from '../redux/features/authSlice';
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: 'min(100vw,400px)',
-    borderRadius: '0.5rem',
-    backgroundColor: 'white',
-  },
-  overlay: {
-    backgroundColor: '#00000063',
-  },
-};
+import customStyles from '../herlpers/modalStyles';
+
 const Signup = () => {
   const { userConnected, error, loading } = useSelector((state) => ({
     ...state.auth,
